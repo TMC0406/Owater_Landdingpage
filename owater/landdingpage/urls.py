cd8e5e80d84f084a6db8a974d4358519aeac2a13
CuongTran <116560620+TMC0406@users.noreply.github.com>
2024-11-30 12:05:41 +0700

    30_11_2024

--- owater/landdingpage/urls.py
+++ owater/landdingpage/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
-from .views import contact, home
+from .views import contact, detail, home, order, products
 urlpatterns = [
     path('', home, name='home'),
     path('contact', contact, name='contact'),
+     path('products', products, name='products'),
+    path('detail/<str:type>', detail, name='detail'),
+    path('order', order, name='order'),
 ]
---
+++ owater/landdingpage/utilities.py
@@ -0,0 +1,14 @@
+water_data = [
+    {"name": "OWA HIGH OXYGEN TYPE A3",
+    "pH_range": "8.0-8.5", 
+    "type": "A3",
+    },
+    {"name": "OWA HIGH OXYGEN TYPE A2",
+    "pH_range": "8.5-8.9", 
+    "type": "A2",
+    },
+    {"name": "OWA HIGH OXYGEN TYPE A1",
+    "pH_range": "9.0-9.5", 
+    "type": "A1",
+    }
+]
